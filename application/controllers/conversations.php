bafd08c045a3a066694c43481dfd0733145ebc08
joshpaulchan <joshpaulchan@gmail.com>
2017-01-24 14:34:18 -0500

    Add paginated index function

--- application/controllers/conversations.php
+++ application/controllers/conversations.php
@@ -3,23 +3,21 @@
 class Conversations extends CI_Controller {
 
 	/**
-	 * Index Page for this controller.
-	 *
-	 * Maps to the following URL
-	 * 		http://example.com/index.php/welcome
-	 *	- or -
-	 * 		http://example.com/index.php/welcome/index
-	 *	- or -
-	 * Since this controller is set as the default controller in
-	 * config/routes.php, it's displayed at http://example.com/
-	 *
-	 * So any other public methods not prefixed with an underscore will
-	 * map to /index.php/welcome/<method_name>
-	 * @see http://codeigniter.com/user_guide/general/urls.html
-	 */
-	public function index()
-	{
-		$this->load->view('welcome_message');
+	* Initialize the database classes and class attributes.
+	*
+	* @post 	: prevents caching
+	*
+	**/
+	public function __construct() {
+		parent::__construct();
+
+		// load db model
+		$this->load->model('conversations_model');
+
+		// prevent caching
+		$this->output->set_header('Last-Modified: ' . gmdate("D, d M Y H:i:s") . ' GMT');('Cache-Control: no-store, no-cache, must-revalidate, post-check=0, pre-check=0');
+		$this->output->set_header('Pragma: no-cache');
+		$this->output->set_header("Expires: Mon, 26 Jul 1997 05:00:00 GMT");
 	}
 
 	/**
@@ -32,16 +30,37 @@ class Conversations extends CI_Controller {
 	* @param	: string	: q		: the number of items to return per page
 	* @return	: array 	: object with conversation results
 	**/
-	public function list($page=0, $n=0, $q='') {
-		$conversations = [];
-		return [
+	public function index($page=0, $n=25, $q='') {
+		// figure out skips
+		$num_convos = $this->conversations_model->count();
+		$total_num_pages = ceil($num_convos / $n);
+
+		// fetch conversations
+		$conversations = $this->conversations_model->get_page($page, $n);
+		// $conversations = [];
+
+		// create next page link
+		$next_str = null;
+		if (($page + 1) <= $total_num_pages) {
+			$next_str = "/conversations?page=".($page + 1)."&n=".$n;
+		}
+
+		//  create prev page link
+		$prev_str = null;
+		if ($page > 0) {
+			$prev_str = "/conversations?page=".($page - 1)."&n=".$n;
+		}
+
+		$data = [
 			"per_page"		=> $n,
 			"current_page"	=> $page,
-			"next_page_url" => "/conversations?page=".($n + 1)."&n=".$n,
-			"prev_page_url" => "/conversations?page=".($n - 1)."&n=".$n,
+			"next_page_url" => $next_str,
+			"prev_page_url" => $prev_str,
 			"data" 			=> $conversations
 		];
 
+		echo $this->_send_json($data);
+		return;
 	}
 
 	public function create() {
@@ -50,6 +69,16 @@ class Conversations extends CI_Controller {
 
 	public function update() {
 
+	}
+
+	private function _send_json($data) {
+		// log vars
+		// echo var_dump($data);
+		// return;
+
+		// format response
+		$this->output->set_content_type('application/json');
+		return json_encode($data);
 	}
 }
 
