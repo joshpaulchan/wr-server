d660c39e958d90e2a7e0e2c9aa470a612d49f021
joshpaulchan <joshpaulchan@gmail.com>
2017-01-24 15:47:04 -0500

    Add test function to welcome controller

--- application/controllers/welcome.php
+++ application/controllers/welcome.php
@@ -2,15 +2,24 @@
 
 class Welcome extends CI_Controller {
 
+	public function __construct() {
+		parent::__construct();
+		
+		// prevent caching
+		$this->output->set_header('Last-Modified: ' . gmdate("D, d M Y H:i:s") . ' GMT');('Cache-Control: no-store, no-cache, must-revalidate, post-check=0, pre-check=0');
+		$this->output->set_header('Pragma: no-cache');
+		$this->output->set_header("Expires: Mon, 26 Jul 1997 05:00:00 GMT");
+	}
+
 	/**
 	 * Index Page for this controller.
 	 *
 	 * Maps to the following URL
 	 * 		http://example.com/index.php/welcome
-	 *	- or -  
+	 *	- or -
 	 * 		http://example.com/index.php/welcome/index
 	 *	- or -
-	 * Since this controller is set as the default controller in 
+	 * Since this controller is set as the default controller in
 	 * config/routes.php, it's displayed at http://example.com/
 	 *
 	 * So any other public methods not prefixed with an underscore will
@@ -20,6 +29,10 @@ class Welcome extends CI_Controller {
 	public function index()
 	{
 		$this->load->view('welcome_message');
+	}
+
+	public function test() {
+		echo phpinfo();
 	}
 }
 
