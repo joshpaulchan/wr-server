b73d2f93db67d54a3749d178ef9867c580f93992
joshpaulchan <joshpaulchan@gmail.com>
2017-02-28 14:01:49 -0500

    Fix query string accessing

--- application/controllers/users.php
+++ application/controllers/users.php
@@ -15,40 +15,47 @@ class Users extends CI_Controller {
 		$this->load->model('users_model');
 
 		// prevent caching
-		$this->output->set_header('Last-Modified: ' . gmdate("D, d M Y H:i:s") . ' GMT');('Cache-Control: no-store, no-cache, must-revalidate, post-check=0, pre-check=0');
+		$this->output->set_header('Last-Modified: ' . gmdate("D, d M Y H:i:s") . ' GMT');
+		$this->output->set_header('Cache-Control: no-store, no-cache, must-revalidate, post-check=0, pre-check=0');
 		$this->output->set_header('Pragma: no-cache');
 		$this->output->set_header("Expires: Mon, 26 Jul 1997 05:00:00 GMT");
 	}
 
 	/**
-	* Retrieve a page of users (or queried users).
+	* Retrieve a page of users (approved/unapproved).
 	*
 	* @post : returns a JSON object with links to next queries/pages and pages themselves
 	*
-	* @param	: int		: page	: the specific page of users to load
-	* @param	: int		: n		: the number of items to return per page
-	* @param	: string	: q		: the number of items to return per page
-	* @return	: array 	: object with user results
+	* @param	: bool		: approval	: the approval status to filter for
+	* @param	: int		: page		: the specific page of users to load
+	* @param	: int		: n			: the number of items to return per page
+	* @return	: array 	: array with user objects
 	**/
-	public function index($page=0, $n=25, $q='') {
+	public function index() {
+		// access vars
+		$approv_str = $this->input->get('approval', '');
+		$approval = ($approv_str === 'true');
+		$page = (int)$this->input->get('page', 0);
+		$n = (int)$this->input->get('n', 25);
+
 		// figure out skips
-		$num_convos = $this->users_model->count();
+		$num_convos = $this->users_model->count($approval);
 		$total_num_pages = ceil($num_convos / $n);
 
 		// fetch users
-		$users = $this->users_model->get_page($page, $n);
+		$users = $this->users_model->get_page_with_approval($approval, $page, $n);
 		$users = array_map(array($this, '_strip_pw'), $users);
 
 		// create next page link
 		$next_str = null;
 		if (($page + 1) <= $total_num_pages) {
-			$next_str = "/users?page=".($page + 1)."&n=".$n;
+			$next_str = "/users?page=".($page + 1)."&n=".$n."&approval=".$approv_str;
 		}
 
 		//  create prev page link
 		$prev_str = null;
 		if ($page > 0) {
-			$prev_str = "/users?page=".($page - 1)."&n=".$n;
+			$prev_str = "/users?page=".($page - 1)."&n=".$n."&approval=".$approv_str;
 		}
 
 		$data = array(
