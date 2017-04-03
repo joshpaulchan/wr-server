2574e5c64b62913b52a2c91348e16995cbe19759
joshpaulchan <joshpaulchan@gmail.com>
2017-04-03 15:13:59 -0400

    Add wr system config file

---
+++ application/config/wr_config.php
@@ -0,0 +1,14 @@
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+/*
+|--------------------------------------------------------------------------
+| Emails
+|--------------------------------------------------------------------------
+|
+|
+*/
+$config['webresponse_email'] = 'webstaff.rutgers.edu';
+
+
+/* End of file constants.php */
+/* Location: ./application/config/wr_config.php */
