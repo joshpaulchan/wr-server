bcba1c760e4192673235d61fb7e4cf4680fd27fb
joshpaulchan <joshpaulchan@gmail.com>
2017-04-03 16:18:11 -0400

    Implement full create_reply

--- application/controllers/conversation.php
+++ application/controllers/conversation.php
@@ -1,5 +1,7 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
+use SimpleValidator\Validator;
+
 class Conversation extends MY_Controller {
 
 	/**
@@ -16,6 +18,9 @@ class Conversation extends MY_Controller {
 
 		// load email lib
 		$this->load->library('email');
+
+		// load config
+		$this->config->load('wr_config');
 	}
 
 	/**
@@ -33,48 +38,66 @@ class Conversation extends MY_Controller {
 	}
 
 	/**
-	* TODO: Create a reply message and send it to the other participant of the conversation.
+	* Create a reply message and send it to the other participant of the conversation.
 	*
-	* @post : returns a JSON object with conversation details and messages
+	* @post : [success] returns a JSON object with conversation details and messages
 	*
 	* @param	: int		: id	: the specific page of conversations to reply to
 	* @param	: string	: body	: the body of the reply message
 	* @return	: array 	: object with conversation and its messages if it exists
 	**/
-	public function create($id, $body) {
+	public function create($id) {
+		$json_data = json_decode(file_get_contents('php://input'), TRUE);
+
+		// validate form
+		$rules = array(
+			// body: 4-256 characters long, and plain text
+			"body" => array("required", "max_length(256)", "min_length(4)"),
+		);
+		$result = Validator::validate($json_data, $rules);
+
+		if ($result->isSuccess() == false) {
+			return $this->_send_json(array(
+				"error" => true,
+				"message" => $result->getErrors()
+			));
+		}
+
+		$body = $json_data['body'];
 		$convo = $this->conversations_model->get_conversation($id);
 
 		// set email config
 		$config = [
-			'userAgent'	=> 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.87 Safari/537.36',
+			'userAgent'	=> $ip = $this->input->ip_address(),
 			'mailtype'	=> 'html',
 			'crlf'		=> '\r\n',
 			'newline'	=> '\r\n'
 		];
-		$this->email->initialize($email_config);
+		// $this->email->initialize($config);
 
 		// configure email
-		$our = [
-			'email'	=> 'webstaff@ucm.rutgers.edu',
+		$our = array(
+			'email'	=> $this->config->item('webresponse_email'),
 			'name'	=> 'Rutgers UCM Web Staff'
-		];
-		// FIXME: import a template for the body
+		);
+
+		// TODO: import a template for the body
 		$full_body = $body;
 
 		// send email
-		$this->email->from($our->email, $our->name);
-		$this->email->to($convo->emailFrom);
-		$this->email->subject('['.$convo->id.'] - '.$convo->subject);
+		$this->email->from($our['email'], $our['name']);
+		$this->email->to($convo['emailFrom']);
+		$this->email->subject('['.$convo['id'].'] - '.$convo['subject']);
 		$this->email->message($full_body);
-		$this->email->send();
+		// $this->email->send();
 
 		// create record
-		$this->conversations_model->create_reply(
+		return $this->_send_json($this->conversations_model->create_reply(
 			$id,
-			$our->email,
-			$convo->emailFrom,
+			$convo['emailFrom'],
+			$our['email'],
 			$full_body
-		);
+		));
 	}
 
 	public function update() {
