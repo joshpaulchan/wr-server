805f85c48839fe7f622f858fccb5a14d084b87ce
joshpaulchan <joshpaulchan@gmail.com>
2017-04-24 15:19:31 -0400

    Implement conversation forwarding

--- application/controllers/conversation.php
+++ application/controllers/conversation.php
@@ -99,6 +99,90 @@ class Conversation extends MY_Controller {
 		return $this->_send_json($resp);
 	}
 
+	/**
+	* Forward a conversation and send the thread to the specified recipient.
+	*
+	* @post : [success] returns a JSON object with conversation details and messages
+	*
+	* @param	: int		: id		: the specific page of conversations to reply to
+	* @param	: string	: forwardTo	: the email to forward the conversation to
+	* @param	: string	: body		: the body of the reply message
+	* @return	: array 	: object with conversation and its messages if it exists
+	**/
+	public function forward($id) {
+		$json_data = $this->get_json();
+
+		// validate form
+		// forwardTo: 4-128 character long email
+		$this->form_validation->set_rules('forwardTo', 'forwardTo', 'trim|required|min_length[4]|max_length[128]|valid_email');
+		// body: 4-256 characters long, and plain text
+		$this->form_validation->set_rules('body', 'body', 'trim|min_length[4]|max_length[256]');
+		$result = $this->form_validation->run();
+
+		if ($result == false) {
+			return $this->_send_json(array(
+				"error" => true,
+				"message" => validation_errors()
+			));
+		}
+
+		$forwardTo = $json_data['forwardTo'];
+		$body = (array_key_exists('body', $json_data) ? $json_data['body'] : '');
+		$convo = $this->conversations_model->get_conversation($id);
+
+		$our = array(
+			'email'	=> $this->config->item('webresponse_email'),
+			'name'	=> $this->config->item('webresponse_name')
+		);
+
+		// concatenate thread to string and forward to
+		$full_body = $body.$this->_stringify_thread($convo['messages']);
+		// echo $full_body;
+		// return;
+
+		// send email
+		$this->email->from($our['email'], $our['name']);
+		$this->email->to($forwardTo);
+		$this->email->subject('fwd: ['.$convo['id'].'] - '.$convo['subject']);
+		$this->email->message($full_body);
+
+		try {
+			$this->email->send();
+			// TODO: create forward record
+			// $resp = $this->conversations_model->create_reply(
+			// 	$id,
+			// 	$convo['emailFrom'],
+			// 	$our['email'],
+			// 	$full_body
+			// );
+			$resp = array(
+				'error' => false
+			);
+		} catch (Exception $e) {
+			$resp = array(
+				"error" => true,
+				"message" => "There was an error forwarding this message."
+			);
+		}
+		return $this->_send_json($resp);
+	}
+
+	// Turn list of message objects into a giant string
+	//
+	private function _stringify_thread($messages) {
+		$s = '';
+
+		$s .= "<br/><br/><span>Begin forwarded message:</span>"; //
+
+		foreach ($messages as $msg) {
+			$s .= "<br/><br/><span>==========</span> "; // top delimiter (2 newlines and 10 ='s')
+			$s .= "<span>&lang;".$msg['emailFrom']."&rang;'s message at ".$msg['createdAt']."</span><br/><br/>";
+			$s .= $msg['body'];
+		}
+
+		return $s;
+	}
+
 	public function update() {
 
 	}
